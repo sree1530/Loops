78dce4c54066a9a74c1deb4c9be5198a45da12fd
sree1530 <129590411+sree1530@users.noreply.github.com>
2023-04-01 15:48:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log('Hello World');
+//for loop
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+// for(let i=10;i>=0;i--){
+//     console.log(i);
+// }
+//sum of natural numbers
+// let sum=0;
+// for(let i=1;i<=10;i++){
+//     sum=sum+i;
+// }
+//console.log('sum of 10 natural numbers are',sum);
+//while loop
+// let i=0;
+// while (i<=10) {
+//     console.log(i);
+//     i++;
+// }
+//do....while
+let i=2;
+do {
+    console.log(i)
+} while (i>2)
